d6dd4be13f30eec44769766ded10b343302a4276
sanket-prajapati <sanketprajapati0209@gmail.com>
2024-07-03 03:56:02 +0000

    Added translation in php file.

--- web/modules/custom/tit/src/Controller/TitController.php
+++ web/modules/custom/tit/src/Controller/TitController.php
@@ -16,9 +16,10 @@ final class TitController extends ControllerBase {
    */
   public function __invoke(): array {
 
+    $var = "Passing var in translating string";
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+      '#markup' => $this->t('Welcome! @var', ['@var' => $var]),
     ];
 
     return $build;
