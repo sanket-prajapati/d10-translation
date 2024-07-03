fbcd1bee2798e06b25b45b3555f4e7c7b2ee3f82
sanket-prajapati <sanketprajapati0209@gmail.com>
2024-07-03 17:57:50 +0000

    Updated module for UIT

--- web/modules/custom/tit/src/Controller/TitController.php
+++ web/modules/custom/tit/src/Controller/TitController.php
@@ -5,24 +5,35 @@ declare(strict_types=1);
 namespace Drupal\tit\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Returns responses for Test Interface transaltion routes.
  */
-final class TitController extends ControllerBase {
+class TitController extends ControllerBase {
+  use StringTranslationTrait;
 
   /**
    * Builds the response.
    */
   public function __invoke(): array {
 
-    $var = "Passing var in translating string";
-    $build['content'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('Welcome! @var', ['@var' => $var]),
+    // $var = "Passing var in translating string";
+    // $build['content'] = [
+    //   '#type' => 'item',
+    //   '#markup' => t('Welcome! @var'),
+    // ];
+
+    return [
+      '#theme' => 'tit_page',
+      '#attached' => [
+        'library' => [
+          'tit/global-scripts',
+        ],
+      ],
     ];
 
-    return $build;
+    // return $build;
   }
 
 }
